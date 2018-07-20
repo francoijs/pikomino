534225a5db57f435b3b9515eceb65bd7391fac6a
Francois Schneider <francoijs@users.noreply.github.com>
2018-07-20 22:52:44 +0200

    game: allow stopping prematurely and still get the statistics for the games played

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import logging, argparse
+import logging, argparse, signal
 from strategy import episode, s_setparams
 
 
@@ -10,6 +10,7 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s %(message)s",
 )
 log = logging.getLogger('main')
+running = True
 
 
 def main():
@@ -41,25 +42,36 @@ def main():
 
     # game on
     log.info('playing <%s> against <%s>...', q1.fname, 'itself' if q1==q2 else q2.fname)
-    wins_left = wins_right = draws = 0
+    wins_left = wins_right = draws = played = 0
     for game in range(args.games):
-        reward,state,_,rounds = episode(q1, q2)
+        state,_,rounds = episode(q1, q2)
         log.info('game %d: rounds=%3d, winner=%s, score=%d/%d',
                  game, rounds,
                  'left' if state.player_wins() else 'right' if state.opponent_wins() else 'draw',
                  state.player_score(), state.opponent_score()
         )
+        played += 1
         if state.player_wins():
             wins_left += 1
         elif state.opponent_wins():
             wins_right += 1
         else:
             draws += 1
+        if not running:
+            break
     log.info('stats: left %d%% / right %d%% / draw %d%%',
-             wins_left*100/args.games,
-             wins_right*100/args.games,
-             draws*100/args.games
+             wins_left*100/played,
+             wins_right*100/played,
+             draws*100/played
     )
 
+def stop(_signum, _frame):
+    log.info('stopping...')
+    global running
+    running = False
+    
 if __name__ == "__main__":
+    # sigterm
+    signal.signal(signal.SIGINT, stop)
+    signal.signal(signal.SIGTERM, stop)
     main()
